fae31ed38d7005b84990ec69d3a49417baf18193
takenoko <takenoko.tech@gmail.com>
2019-04-21 18:51:37 +0900

    init commit.

---
+++ src/sample/index.tsx
@@ -0,0 +1,2 @@
+import THREE from "three";
+import AR from "ar.js";
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+var development = {
+  entry: {
+    sample: "./src/sample/index",
+    vrm: "./src/webar-vrm/index"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  devtool: "inline-source-map"
+};
+
+var production = {
+  mode: "production",
+  entry: {
+    vrm: "./src/webar-vrm/index"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: ".", to: ".", ignore: ["!*.css"] }], {
+      context: "static/css"
+    })
+  ]
+};
+
+if ((process.env.NODE_ENV || "").trim() != "production") {
+  console.log("NODE_ENV", "development");
+  module.exports = development;
+} else {
+  console.log("NODE_ENV", "production");
+  module.exports = production;
+}
